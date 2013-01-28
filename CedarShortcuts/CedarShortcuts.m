df8964dcbb2daeb6f43d8d873cbc2e9c79574464
Dmitriy Kalinin <cppforlife@gmail.com>
2013-01-27 16:28:00 -0800

    be extra cautions about being NSNotificationCenter observer

--- CedarShortcuts/CedarShortcuts.m
+++ CedarShortcuts/CedarShortcuts.m
@@ -39,6 +39,7 @@
 }
 
 - (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
     self.runFocusedMenu = nil;
     self.openAlternateMenu = nil;
     self.editMenu = nil;
