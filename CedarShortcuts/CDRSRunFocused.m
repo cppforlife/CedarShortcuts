88dd611eda67f6017f0d0923c85c310703a1c318
Dmitriy Kalinin <cppforlife@gmail.com>
2013-01-27 16:32:55 -0800

    refactor lastFocusedRun

--- CedarShortcuts/CDRSRunFocused.m
+++ CedarShortcuts/CDRSRunFocused.m
@@ -15,40 +15,38 @@
 
 @implementation CDRSRunFocused
 
-static NSString *__lastFocusedRun = nil;
-
 - (BOOL)runFocusedSpec {
-    return [self _run:F(@"%@:%lld", self._currentFilePath, self._currentLineNumber)];
+    self.lastFocusedRunPath = F(@"%@:%lld", self._currentFilePath, self._currentLineNumber);
+    return [self _runFilePathAndLineNumber:self.lastFocusedRunPath];
 }
 
 - (BOOL)runFocusedFile {
-    return [self _run:F(@"%@:0", self._currentFilePath)];
+    self.lastFocusedRunPath = F(@"%@:0", self._currentFilePath);
+    return [self _runFilePathAndLineNumber:self.lastFocusedRunPath];
 }
 
 - (BOOL)runFocusedLast {
-    return __lastFocusedRun ? [self _run:__lastFocusedRun] : NO;
+    if (self.lastFocusedRunPath) {
+        return [self _runFilePathAndLineNumber:self.lastFocusedRunPath];
+    } return NO;
 }
 
-- (BOOL)_run:(NSString *)filePathAndLineNumber {
+#pragma mark -
+
+- (BOOL)_runFilePathAndLineNumber:(NSString *)filePathAndLineNumber {
     if (!filePathAndLineNumber) return NO;
 
     static NSString *CDRSRunFocused_EnvironmentVariableName = @"CEDAR_SPEC_FILE";
 
     [IDELaunchSession_CDRSCustomize customizeNextLaunchSession:^(id launchSession){
         NSLog(@"CedarShortcuts - Running spec: %@", filePathAndLineNumber);
-
-        // retain then release since it could be itself
-        [filePathAndLineNumber retain];
-        [__lastFocusedRun release];
-        __lastFocusedRun = filePathAndLineNumber;
-
         id params = [launchSession launchParameters];
 
-        // Used with 'Run' context
+        // Used with 'Run' context (i.e. separate Test target)
         id runEnv = [params environmentVariables];
         [runEnv setObject:filePathAndLineNumber forKey:CDRSRunFocused_EnvironmentVariableName];
 
-        // Used with 'Test' context
+        // Used with 'Test' context (i.e. Test Bundles)
         NSMutableDictionary *testEnv = [[params testingEnvironmentVariables] mutableCopy];
         [testEnv setObject:filePathAndLineNumber forKey:CDRSRunFocused_EnvironmentVariableName];
         [params setTestingEnvironmentVariables:testEnv];
@@ -63,6 +61,7 @@ static NSString *__lastFocusedRun = nil;
 }
 
 #pragma mark - Editor
+#pragma mark - Editor's file path & line number
 
 - (NSString *)_currentFilePath {
     NSString *fullPath = [[self._currentSourceCodeDocument fileURL] absoluteString];
@@ -73,6 +72,20 @@ static NSString *__lastFocusedRun = nil;
     return [[CDRSXcode currentSourceCodeEditor] _currentOneBasedLineNubmer];
 }
 
+#pragma mark - Last focused run path
+
+static NSString *__lastFocusedRunPath = nil;
+
+- (NSString *)lastFocusedRunPath {
+    return __lastFocusedRunPath;
+}
+
+- (void)setLastFocusedRunPath:(NSString *)path {
+    NSString *lastPath = __lastFocusedRunPath;
+    __lastFocusedRunPath = [path copy];
+    [lastPath release];
+}
+
 #pragma mark - Workspace
 
 - (id)_currentSourceCodeDocument {
@@ -80,9 +93,12 @@ static NSString *__lastFocusedRun = nil;
     return [[CDRSXcode currentSourceCodeEditor] sourceCodeDocument];
 }
 
+- (id)_currentWorkspace {
+    return [[CDRSXcode currentWorkspaceController] valueForKey:@"_workspace"];
+}
+
 - (id)_currentScheme {
-    id workspace = [[CDRSXcode currentWorkspaceController] valueForKey:@"_workspace"];
-    id runContextManager = [workspace runContextManager];           // IDEWorkspace
-    return [runContextManager activeRunContext];                    // IDEScheme
+    id runContextManager = [self._currentWorkspace runContextManager]; // IDEWorkspace
+    return [runContextManager activeRunContext];                       // IDEScheme
 }
 @end
